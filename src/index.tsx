f67971f6293cbcdda88248d36849980bc6b8d92d
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-04-23 15:10:28 +0200

    Add example of adding types to functional component

--- src/index.tsx
+++ src/index.tsx
@@ -5,35 +5,9 @@ interface AppProps {
   color?: string;
 }
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
